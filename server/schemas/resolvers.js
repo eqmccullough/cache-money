a4c738c1c0c310f31b0f069026d75598ba441b48
Derick Stadler <derickstadler@gmail.com>
2023-07-31 14:56:24 -0400

    commit changes before pulling over new merge

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,12 +4,12 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        categories: async (parent, { username }) => {
+        profile: async (parent, { username }) => {
             const params = username ? { username }
         },
         category: async (parent, { categoryId }) => {
             return Category.findOne({ _id: categoryId })
-        }
+        },
         me: async (parent, args, context) => {
             if (context.profile) {
                 return Profile.findOne({ _id: context.profile._id}).populate('categories')
