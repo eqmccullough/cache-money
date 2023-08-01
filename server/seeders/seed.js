7969f87399a25d1bea0008320556308a582d33e9
OutlawDisco <aoutlaw00@gmail.com>
2023-08-01 09:55:58 -0600

    combined User and Profile models & reworked Seed data

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,3 +1,39 @@
 const db = require("../config/connection");
-const { Profile, Categories } = require("../models");
-const profileSeeds = require("./profileSeeds.json");
+const { User, Categories, Item } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const categorySeeds = require("./categorySeeds.json");
+const itemSeeds = require("./itemSeeds.json");
+
+db.once("open", async () => {
+  await User.deleteMany({});
+  await Categories.deleteMany({});
+  await Item.deleteMany({});
+
+  for (let i = 0; i < userSeeds.length; i++) {
+    const element = userSeeds[i];
+    //create user
+    const user = await User.create(userdata[i]);
+    //create category
+    const category = await Category.creat(categoryData[i]);
+  }
+  //updateuser with new categories id number
+  const updateUser = await User.findByIdAndUpdate(
+    user.id,
+    { $addToSet: { categories: category._id } },
+    { new: true }
+  );
+  //create item
+  const item = await Item.create(itemData[i]);
+  // add item to category
+  const updateCategory = await Category.findByIdAndUpdate(category._id, {
+    $addToSet: { items: item._id },
+  });
+  // console log everything to make sure it worked
+});
+
+const categories = await Category.insertManu(categorySeeds);
+const profiles = await Profile.insertMany(profileSeeds);
+const items = await Item.insertMany(itemSeeds);
+
+console.log("Database seeded!");
+process.exit(0);
