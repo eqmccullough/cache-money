f891caba5a0ddb36d8216ee915c6a4c600fd81c4
Ben laptop <b.connolly1088@gmail.com>
2023-08-01 17:30:23 -0600

    lots of bug fixes auth.js, typedefs, resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,7 @@
-const { gql } = require('apollo-server-express');
+// const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
+// const typeDefs = gql`
+const typeDefs = `
   type User {
     _id: ID
     username: String
