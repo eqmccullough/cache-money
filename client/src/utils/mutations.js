103e5c607e2c390be8cf870ac0dae483daafa550
OutlawDisco <aoutlaw00@gmail.com>
2023-08-02 14:24:36 -0600

    updated mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client'
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
@@ -13,9 +13,54 @@ export const LOGIN_USER = gql`
 `;
 
 export const ADD_USER = gql`
-mutation Mutation($username: String!, $email: String!, $password: String!, $income: Float!) {
-  addUser(username: $username, email: $email, password: $password, income: $income) {
-    token
+  mutation Mutation(
+    $username: String!
+    $email: String!
+    $password: String!
+    $income: Float!
+  ) {
+    addUser(
+      username: $username
+      email: $email
+      password: $password
+      income: $income
+    ) {
+      token
+    }
   }
-}
+`;
+
+export const ADD_CATEGORY = gql`
+  mutation Mutation($name: String!) {
+    addCategory(name: $name) {
+      _id
+      name
+    }
+  }
+`;
+
+export const ADD_ITEM = gql`
+mutation AddItem($name: String! $amount Float!, $categoryId: ID!) {
+  addItem(name: $name, amount: $amount, categoryId: $categoryId) {
+    _id
+    amount
+    name
+    userId {
+      _id
+      username
+    }
+  }
+}`;
+
+export const UPDATE_ITEM = gql`
+  mutation Mutation($itemId: ID!, $newAmount: Float!) {
+    updateItem(itemId: $itemId, newAmount: $newAmount) {
+      _id
+      name
+      amount
+      userId {
+        _id
+      }
+    }
+  }
 `;
