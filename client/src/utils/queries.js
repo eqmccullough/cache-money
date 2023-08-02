43fdc7de52c610e57761377196d8feb242e765c1
Derick Stadler <derickstadler@gmail.com>
2023-08-02 14:09:50 -0400

    commiting before merges pull down

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,24 @@
 //  put in front end queries
+import { gql } from "@apollo/client";
+
+export const ALL_CATEGORIES = gql`
+  query allCategories {
+    allCategories {
+      _id
+      name
+    }
+  }
+`;
+
+export const CATEGORY_ITEMS = gql`
+  query categoryItems($categoryId: ID!) {
+    categoryItems(categoryId: $categoryId) {
+      name
+      items {
+        _id
+        name
+        amount
+      }
+    }
+  }
+`;
