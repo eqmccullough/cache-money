5434007477630bff23c55718eeda4f0f86c23502
OutlawDisco <aoutlaw00@gmail.com>
2023-08-02 08:28:29 -0600

    query categories that match user id

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,64 +1,67 @@
 // const { AuthenticationError } = require('apollo-server-express')
-const { 
-    Category, 
-    Item, 
-    // Profile, 
-    User 
-} = require('../models');
-const { signToken, AuthenticationError } = require('../utils/auth');
+const {
+  Category,
+  Item,
+  // Profile,
+  User,
+} = require("../models");
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        // categories: async () => {
-        //     return Category.find().populate('')
-        // },
-        // categoryItems: async (parent, { name }, context) => {
-        //     return Category.findOne({ name: name })
-        // }, 
-        me: async (parent, args, context) => {
-            // console.log(context);
-            if (context.user._id) {
-                return User.findOne({ _id: context.user._id}).populate({ 
-                    path: "categories",
-                    populate: {
-                        path: "items",
-                        match: {
-                            userId: {
-                                $eq: context.user._id,
-                            }
-                        }
-                    }
-                })
-            }
-            throw new AuthenticationError('You need to be logged in!');
-        },
+  Query: {
+    categories: async (parent, args, context) => {
+      console.log(context.user);
+      return Category.find().populate({
+        path: "items",
+        match: { userId: { $eq: context.user._id } },
+      });
     },
+    // categoryItems: async (parent, { name }, context) => {
+    //     return Category.findOne({ name: name })
+    // },
+    me: async (parent, args, context) => {
+      // console.log(context);
+      if (context.user._id) {
+        return User.findOne({ _id: context.user._id }).populate({
+          path: "categories",
+          populate: {
+            path: "items",
+            match: {
+              userId: {
+                $eq: context.user._id,
+              },
+            },
+          },
+        });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
 
-    Mutation: {
-        addUser: async (parent, { username, email, password, income }) => {
-            const user = await User.create({ username, email, password, income })
-            const token = signToken(user);
-            return { token, user }
-        },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+  Mutation: {
+    addUser: async (parent, { username, email, password, income }) => {
+      const user = await User.create({ username, email, password, income });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
 
-            if(!user) {
-                throw new AuthenticationError('No user found with this email');
-            }
+      if (!user) {
+        throw new AuthenticationError("No user found with this email");
+      }
 
-            const correctPw = await user.isCorrectPassword(password);
+      const correctPw = await user.isCorrectPassword(password);
 
-            if(!correctPw) {
-                throw new AuthenticationError('Incorrect credentials')
-            }
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
 
-            const token = signToken(user);
+      const token = signToken(user);
 
-            return { token, user };
-        },
-
-    }
-}
+      return { token, user };
+    },
+  },
+};
 
 module.exports = resolvers;
