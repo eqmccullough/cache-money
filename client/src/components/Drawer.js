c879bbe506889e8681699087621d3531a53a8598
Derick Stadler <derickstadler@gmail.com>
2023-08-03 12:41:05 -0400

    new styling added to item rendering

--- client/src/components/Drawer.js
+++ client/src/components/Drawer.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import CategorySelect from "./CategorySelect";
-import { useQuery } from "@apollo/client"
-import { ALL_CATEGORIES, CATEGORY_ITEMS, USER_ITEMS } from "../utils/queries"
+import { useQuery } from "@apollo/client";
+import { ALL_CATEGORIES, CATEGORY_ITEMS, USER_ITEMS } from "../utils/queries";
 import { useParams } from "react-router-dom";
 import ItemList from "./ItemList";
 import Box from "@mui/material/Box";
@@ -19,26 +19,29 @@ import TextField from "@mui/material/TextField";
 import Fab from "@mui/material/Fab";
 import AddIcon from "@mui/icons-material/Add";
 import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from '@mui/icons-material/Delete';
-
-
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export default function Drawer() {
-
-  const { data: allCatData, loading: allCatLoading, error: allCatError } = useQuery(ALL_CATEGORIES)
+  const {
+    data: allCatData,
+    loading: allCatLoading,
+    error: allCatError,
+  } = useQuery(ALL_CATEGORIES);
   const categories = allCatData?.allCategories || [];
 
   // const { data: catItemsData, loading: catItemsLoading, error: catItemsError } = useQuery(CATEGORY_ITEMS)
-  
 
-  const { data: userItemData, loading: userItemLoading, error: userItemError } = useQuery(USER_ITEMS)
+  const {
+    data: userItemData,
+    loading: userItemLoading,
+    error: userItemError,
+  } = useQuery(USER_ITEMS);
 
   const { categoryId } = useParams();
 
   // const { data: catItemsData, loading: catItemsLoading, error: catItemsError } =
   //   useQuery(CATEGORY_ITEMS, {variables: {categoryId: categoryId}})
   // const categoryItems = catItemsData?.categoryItems || [];
-
 
   const [state, setState] = React.useState({
     top: false,
@@ -72,7 +75,10 @@ export default function Drawer() {
     <div>
       {["left"].map((anchor) => (
         <React.Fragment key={anchor}>
-          <Button onClick={toggleDrawer(anchor, true)} id="drawer-pull"> What's your budgie? </Button>
+          <Button onClick={toggleDrawer(anchor, true)} id="drawer-pull">
+            {" "}
+            What's your budgie?{" "}
+          </Button>
           <SwipeableDrawer
             anchor={anchor}
             open={state[anchor]}
@@ -80,23 +86,25 @@ export default function Drawer() {
             onOpen={toggleDrawer(anchor, true)}
           >
             <div>
-              <CategorySelect 
-                categories={categories}
-              />
+              <CategorySelect categories={categories} />
 
               <ul>
-              <li id="custom-input">
+                {/* <li id="custom-input"> */}
                   {/* <ItemList categoryItems={ categoryItems } /> */}
-                  <Fab color="secondary" sx={{ m: 1, bgcolor:"#6048a3"}} aria-label="edit">
+                  {/* <Fab color="secondary" sx={{ m: 1, bgcolor:"#6048a3"}} aria-label="edit">
                   <EditIcon />
                   </Fab>
                   <Fab color="secondary" sx={{ m: 1, bgcolor:"#9e4848"}} aria-label="edit">
                     <DeleteIcon />
-                  </Fab>
-                </li>
+                  </Fab> */}
+                {/* </li> */}
 
                 <li id="custom-input">
-                  <TextField sx={{ m: 1, width: "50ch" }} label="Add Item" variant="filled" />
+                  <TextField
+                    sx={{ m: 1, width: "50ch" }}
+                    label="Add Item"
+                    variant="filled"
+                  />
                   <FormControl sx={{ m: 1, width: "25ch" }} variant="filled">
                     <InputLabel htmlFor="filled-adornment-amount">
                       Amount
@@ -108,11 +116,14 @@ export default function Drawer() {
                       }
                     />
                   </FormControl>
-                  <Fab color="primary" sx={{ m: 1, bgcolor: "#598C58" }} aria-label="add">
+                  <Fab
+                    color="primary"
+                    sx={{ m: 1, bgcolor: "#598C58" }}
+                    aria-label="add"
+                  >
                     <AddIcon />
                   </Fab>
                 </li>
-
               </ul>
             </div>
           </SwipeableDrawer>
