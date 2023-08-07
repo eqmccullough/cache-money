014ad057632368838a64da15b90b937c99b0a1a6
OutlawDisco <aoutlaw00@gmail.com>
2023-08-07 11:38:36 -0600

    added onChange to selectCatId event

--- client/src/components/CategorySelect.js
+++ client/src/components/CategorySelect.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { useTheme } from '@mui/material/styles';
-import { useQuery } from '@apollo/client';
+import React, { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { useQuery } from "@apollo/client";
 import { useParams } from "react-router-dom";
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
-import ItemList from './ItemList';
-import { CATEGORY_ITEMS } from '../utils/queries';
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import ItemList from "./ItemList";
+import { CATEGORY_ITEMS } from "../utils/queries";
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -21,28 +21,28 @@ const MenuProps = {
   },
 };
 
-
-export default function CategorySelect({ categories }) {
+export default function CategorySelect({ categories, onChange }) {
   // if(!categories) {
   //   return <MenuItem>No Categories</MenuItem>
   // }
   // const theme = useTheme();
   const { categoryId } = useParams();
-  const [categoryName, setCategoryName] = useState(categoryId || '');
-
-  
+  const [categoryName, setCategoryName] = useState(categoryId || "");
 
   const queryOptions = categoryName
-  ? { variables: { categoryId: categoryName } }
-  : undefined;
-  const { data: catItemsData, loading: catItemsLoading, error: catItemsError } = useQuery(CATEGORY_ITEMS, queryOptions)
+    ? { variables: { categoryId: categoryName } }
+    : undefined;
+  const {
+    data: catItemsData,
+    loading: catItemsLoading,
+    error: catItemsError,
+  } = useQuery(CATEGORY_ITEMS, queryOptions);
   console.log(catItemsData);
 
-    // {variables: {categoryId: categoryName}});
+  // {variables: {categoryId: categoryName}});
   const categoryItems = catItemsData?.categoryItems?.items || [];
   // const catName = catItemsData?.categoryItems?.name || [];
   // console.log(catName);
-
 
   const handleChange = (event) => {
     // const { target: { value } } = event;
@@ -51,41 +51,41 @@ export default function CategorySelect({ categories }) {
     // console.log( value);
     // console.log(event.target.value)
     // console.log(categoryItems);
+    onChange(selectedCatId);
     setCategoryName(selectedCatId);
     // console.log(categoryItems);
   };
-  console.log('Category Items:', categoryItems);
+  console.log("Category Items:", categoryItems);
   return (
     <>
-    <div>
-      <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel id="demo-multiple-name-label">Category</InputLabel>
-        <Select
-          labelId="demo-multiple-name-label"
-          id="category-select"
-          value={categoryName}
-          onChange={handleChange}
-          input={<OutlinedInput label="Category" />}
-          MenuProps={MenuProps}
-        >
-          {categories.map((category) => (
-            <MenuItem
-              key={category._id}
-              value={category._id}
-              // style={getStyles(category, categoryName, theme)}
-            >
-              {category.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-    </div>
+      <div>
+        <FormControl sx={{ m: 1, width: 300 }}>
+          <InputLabel id="demo-multiple-name-label">Category</InputLabel>
+          <Select
+            labelId="demo-multiple-name-label"
+            id="category-select"
+            value={categoryName}
+            onChange={handleChange}
+            input={<OutlinedInput label="Category" />}
+            MenuProps={MenuProps}
+          >
+            {categories.map((category) => (
+              <MenuItem
+                key={category._id}
+                value={category._id}
+                // style={getStyles(category, categoryName, theme)}
+              >
+                {category.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
 
-    <div>
-            {/* console.log('Category Items:', categoryItems); */}
-            <ItemList categoryItems={ categoryItems } />
-        
-    </div>
+      <div>
+        {/* console.log('Category Items:', categoryItems); */}
+        <ItemList categoryItems={categoryItems} />
+      </div>
     </>
   );
 }
